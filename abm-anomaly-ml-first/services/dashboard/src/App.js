7b7a87c6884b0428d7dcc1f289a07d7e7360fc8d
pearsoncn <pearsoncn@jncb.com>
2025-07-20 22:33:07 -0500

    Refactor: Update nginx configuration and enhance routing in App component

--- abm-anomaly-ml-first/services/dashboard/src/App.js
+++ abm-anomaly-ml-first/services/dashboard/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
+import RealtimeMonitoringInterface from './RealtimeMonitoringInterface';
+import ExpertLabelingInterface from './ExpertLabelingInterface';
+import MultiAnomalyView from './MultiAnomalyView';
+import ContinuousLearningInterface from './ContinuousLearningInterface';
+import DataViewer from './DataViewer';
 
 function App() {
-  return <Dashboard />;
+  return (
+    <Router basename="/dashboard">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/realtime" element={<RealtimeMonitoringInterface />} />
+        <Route path="/multi-anomaly" element={<MultiAnomalyView />} />
+        <Route path="/expert-labeling" element={<ExpertLabelingInterface />} />
+        <Route path="/continuous-learning" element={<ContinuousLearningInterface />} />
+        <Route path="/data-viewer" element={<DataViewer />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
